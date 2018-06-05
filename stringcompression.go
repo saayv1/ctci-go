62e2bfafa0f55a73bb464d7cbcba2730250aefe2
Vyaas <saayvsaayv@gmail.com>
2018-06-05 16:45:56 -0500

    formatted

--- stringcompression.go
+++ stringcompression.go
@@ -2,33 +2,33 @@ package main
 
 import (
 	"strconv"
-//	"strings"
+	//	"strings"
 	"fmt"
 	"unicode/utf8"
 )
 
 func main() {
 	input := "aabcccccaaa"
-	var compressed string;
+	var compressed string
 	firstAlphabet := string(input[0])
 	count := 0
-	
-	for _,letters := range input {
+
+	for _, letters := range input {
 		if string(letters) == firstAlphabet {
 			count++
 		} else {
 
-			compressed = compressed + firstAlphabet +  strconv.Itoa(count)
+			compressed = compressed + firstAlphabet + strconv.Itoa(count)
 			firstAlphabet = string(letters)
-			count =1
+			count = 1
 		}
 	}
 	compressed = compressed + firstAlphabet + strconv.Itoa(count)
 	l1 := utf8.RuneCountInString(input)
 	l2 := utf8.RuneCountInString(compressed)
-	if l2>l1{
+	if l2 > l1 {
 		fmt.Println(input)
-		return	
+		return
 	}
 	fmt.Println(compressed)
 }
