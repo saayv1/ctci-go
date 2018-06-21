6ff6ac340dc5670aecc603fdefae316a13b9df0e
Vyaas <saayvsaayv@gmail.com>
2018-06-21 00:06:12 -0700

    Linked List chapter's first problem solved

--- linkedListProblems/problem1.go
+++ linkedListProblems/problem1.go
@@ -1,42 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"../linkedList"
+	"fmt"
 )
 
-func DeleteDuplicates(n linkedList.Node)*linkedList.Node{
+func MapCheck(np *linkedList.Node, m map[int]int) map[int]int {
+	x, ok := m[np.Data]
+	if ok {
+		x++
+		m[np.Data] = x
+	} else {
+		m[np.Data] = 1
+	}
+	return m
+}
+
+func DeleteDuplicates(n linkedList.Node) *linkedList.Node {
 	np := &n
 	m := make(map[int]int)
 	np = linkedList.GoBack(*np)
 	for np.Next != nil {
-		x,ok := m[np.Data]
-		if ok {
-			x++;
-			m[np.Data] = x
-		}	else	{
-			m[np.Data] = 1
-		}
+		m = MapCheck(np, m)
 		np = np.Next
 	}
-	   x,ok := m[np.Data]
-                if ok {
-                        x++;
-                        m[np.Data] = x
-                }       else    {
-                        m[np.Data] = 1
-                }
-	for key,value := range m {
-		if value==1 {
-			delete(m,key)
+	m = MapCheck(np, m)
+	for key, value := range m {
+		if value == 1 {
+			delete(m, key)
 		}
 	}
 	fmt.Println(m)
-	for key,_ := range m{
+	for key, _ := range m {
 		np = linkedList.GoBack(*np)
-		for np.Next != nil	{
+		for np.Next != nil {
 			if np.Data == key {
-				if m[key]>1 {
+				if m[key] > 1 {
 					m[key] -= 1
 					np = linkedList.Delete(np)
 				}
@@ -48,21 +47,22 @@ func DeleteDuplicates(n linkedList.Node)*linkedList.Node{
 	return np
 }
 
-
-func main(){
-	n := linkedList.Node{nil,1,nil}
+func main() {
+	n := linkedList.Node{nil, 1, nil}
 	np := &n
-	slice := []int{55,54,55,52,51,55}	
-	linkedList.AddStuffFront(&n,slice)
+	slice := []int{55, 54, 55, 52, 51, 55}
+	linkedList.AddStuffFront(&n, slice)
 	n.AddFront(15)
 	n.AddRear(15)
 	n.AddRear(17)
 	n.AddFront(15)
 	n.AddRear(17)
 	np = linkedList.GoBack(*np)
+	fmt.Println("Input Linked List")
 	linkedList.PrintStuffL2R(*np)
-	fmt.Println("\n\n\n")	
+	fmt.Println("\n\n")
 	np = DeleteDuplicates(*np)
 	np = linkedList.GoBack(*np)
+	fmt.Println("Output Linked List")
 	linkedList.PrintStuffL2R(*np)
 }
