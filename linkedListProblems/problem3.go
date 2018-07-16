01d19f2f419aa0d7d05d25b327d970b557e6c938
Vyaas <saayvsaayv@gmail.com>
2018-07-15 21:18:40 -0500

    added problem 3

---
+++ linkedListProblems/problem3.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"../linkedList"
+	"fmt"
+)
+
+func main() {
+	n := linkedList.Node{nil, 0, nil}
+	for i := 1; i < 100; i++ {
+		n.AddFront(i)
+	}
+
+	k := 14
+	nv := linkedList.GoBack(n)
+	fmt.Println(nv.Data)
+	fmt.Println("deleting the 14th node")
+
+	for i := 0; i < k; i++ {
+		if nv.Next == nil {
+			fmt.Println("cannot delete the kth node")
+			return
+		}
+		nv = nv.Next
+	}
+	nv = linkedList.Delete(nv)
+
+	linkedList.PrintStuffL2R(*nv)
+
+}
